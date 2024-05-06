7722b53c5e4644b75708d943be78549d503fed8d
TahabM <tahabmoazzam@gmail.com>
2024-05-05 22:42:16 -0500

    assignment

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the cafeteria order form!');
-});
 
 app.post('/submit-order', (req, res) => {
-  console.log(req.body); 
-  res.send('Order submitted successfully!');
+});
+
+app.get('/confirmation', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
